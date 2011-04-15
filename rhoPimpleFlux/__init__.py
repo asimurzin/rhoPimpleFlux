85f111cd7ce7b8646dd46e71384c4797527481eb
Andrey Simurzin <asimurzin@gmail.com>
2011-04-15 11:42:25 +0400

    move dispatcher to __init__ and preparation it

---
+++ rhoPimpleFlux/__init__.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+#--------------------------------------------------------------------------------------
+## VulaSHAKA (Simultaneous Neutronic, Fuel Performance, Heat And Kinetics Analysis)
+## Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## 
+## See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka
+##
+## Author : Alexey PETROV
+##
+
+
+#--------------------------------------------------------------------------------------
+from Foam import FOAM_VERSION, FOAM_REF_VERSION, FOAM_BRANCH_VERSION
+if FOAM_REF_VERSION( "==", "010600" ):
+    from rhoPimpleFlux.r1_6 import *
+    pass
+
+   
+#--------------------------------------------------------------------------------------
+if FOAM_BRANCH_VERSION( "dev", ">=", "010600" ):
+    from rhoPimpleFlux.r1_6_dev import *
+    pass
+
+   
+#--------------------------------------------------------------------------------------
+if FOAM_REF_VERSION( ">=", "010700" ):
+    from rhoPimpleFlux.r1_7_0 import *
+    pass
+
+
+#--------------------------------------------------------------------------------------
+def entry_point():
+    try:
+       import sys; argv = sys.argv
+       return main_standalone( len( argv ), argv )
+    except NameError:
+       print
+       print "There is no implementation of the current OpenFOAM version"
+       print
+       pass
+
+
+#--------------------------------------------------------------------------------------
+if __name__ == "__main__" :
+    entry_point()
+    pass
+    
+    
+#--------------------------------------------------------------------------------------
